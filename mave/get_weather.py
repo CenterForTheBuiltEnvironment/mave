661b5835a02a8a3d200c6ef1a038bb69d561a959
Paul Raftery <p.raftery@gmail.com>
2015-09-30 17:33:30 -0700

    fixed datetime parsing

--- mave/get_weather.py
+++ mave/get_weather.py
@@ -20,52 +20,49 @@ class GetWunder(object):
                  geocode = 'SFO',
                  interp_interval = '15m',
                  **kwargs):
+        pdb.set_trace()
         self.target_dts = np.arange(start,
                                     end,
                                     dtype='datetime64[%s]'%interp_interval)
-        self.raw_dts, self.raw_data = self.get_raw(start, end, geocode)
-        self.processed_data = self.process_time(self.target_dts,
-                                                self.raw_dts,
-                                                self.raw_data)
+        self.target_unix = (self. target_dts - \
+                            np.datetime64('1970-01-01T00:00:00Z'))\
+                           /np.timedelta64(1,'s')
+        self.timestamps, self.unix, self.data = \
+                                              self.get_raw(start, end, geocode)
+        self.interp_data = np.interp(self.target_unix,
+                                     self.unix,
+                                     self.data)
 
     def get_raw(self, start, end, geocode):
+        # define a range of dates
         dates = np.arange(start.date(), end.date(), dtype='datetime64[D]')
-        pdb.set_trace()
+        # download the timestamp data for that range of dates
         raw = np.asarray(map(lambda x: self.get_daily(geocode,x),dates))
-        raw_dts = np.hstack(raw[:,0])
-        raw_data = np.hstack(raw[:,1])
-        return raw_dts, raw_data
+        # stack each day of downloaded data
+        data = np.hstack(raw[:,1])
+        timestamps = np.hstack(raw[:,0])
+        # convert to unix time 
+        vec_parse = np.vectorize(self.str_to_unix)
+        unix = vec_parse(timestamps)
+        return timestamps, unix, data
 
-    def get_daily(self,geocode,dt):
-        year = dt.astype('datetime64[Y]').astype(int)+1970
-        month = dt.astype('datetime64[M]').astype(int)%12+1
-        day = int((dt-dt.astype('datetime64[M]'))/np.timedelta64(1,'D'))+1
-        f = urllib2.urlopen('http://www.wunderground.com/history/airport/'\
-           +geocode+'/'+str(year)+'/'+str(month)+'/'+str(day)+'/'\
-           +'DailyHistory.html?format=1')
+    def get_daily(self,geocode,date):
+        date = date.astype(datetime.datetime)
+        url = ('http://www.wunderground.com/history/airport'  
+              '/%s/%s/%s/%s/DailyHistory.html?format=1')%\
+              (geocode,date.year,date.month,date.day)
+        f = urllib2.urlopen(url)
         raw = f.read().splitlines()
-        raw_txt = np.genfromtxt(raw, delimiter=',',\
-                                dtype=None,\
+        raw_txt = np.genfromtxt(raw, 
+                                delimiter=',',
+                                dtype=None,
                                 skip_header=2)
-        time = raw_txt['f0']
-        dt_str = str(dt)[:10]+' '+str(dt)[11:19]
-        time_series = np.ravel(np.core.defchararray.add(dt_str,time))
+        ts = raw_txt['f0']
+        time_series = np.ravel(np.core.defchararray.add(str(date)+' ',ts))
         return time_series, raw_txt['f1']
 
-    def process_time(self, target_dts, current_dts, data):
-        func_p = np.vectorize(dparser.parse)
-        func2 = np.vectorize(self.get_unixtime)
-        try:
-            dt = map(lambda x: time.strptime(x,'%y-%b-%d %I:%M:%S'),current_dts)
-        except:
-            dt = func_p(current_dts)
-        dt_float = func2(dt) 
-        normal_float = (target_dts - np.datetime64('1970-01-01T00:00:00Z'))\
-                       /np.timedelta64(1,'s')
-        target_data = np.interp(normal_float,dt_float,data)
-        return target_data
-
-    def get_unixtime(self,dt):
+    def str_to_unix(self,s):
+        dt = dparser.parse(s)
         secs = time.mktime(dt.timetuple())
         return secs
 
@@ -78,4 +75,4 @@ if __name__ == "__main__":
     print '\nTarget datetimes'
     print test.target_dts
     print '\nInterpolated data'
-    print test.processed_data 
+    print test.interp_data 
