ef1c93897608d7279134729ec3fe42affd862b2b
Paul Raftery <p.raftery@gmail.com>
2016-03-07 09:17:11 -0800

    update version no

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.42',
+    version='0.0.5',
 
     description='Package for Measurement and Verification',
     long_description=long_description,
@@ -101,6 +101,8 @@ setup(
 
     scripts=[
         'bin/mave',
+        'bin/mave-tmy',
+        'bin/mave-weather'
     ]
 
     # Although 'package_data' is the preferred approach, in some case you may
