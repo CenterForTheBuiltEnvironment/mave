b1afa522bde2ed7f838f3f955d4ff3aab29cb001
Paul Raftery <p.raftery@gmail.com>
2016-03-10 04:32:13 -0800

    close #45 by adding a few quick tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,144 +1,101 @@
 import unittest
 import pdb
 import sys
-import datetime 
+from datetime import datetime 
 import numpy as np
 sys.path.insert(0, './mave/')
 sys.path.insert(0, '../mave/')
 import trainers
 from core import Preprocessor, ModelAggregator, MnV
 from comparer import Comparer
+import dataset
 import location
+from nose.tools import assert_equal, assert_true, assert_false, assert_almost_equal
 
 class Test(unittest.TestCase):
 
     EPS = 0.001
-    TEST_PATH_1 = "./mave/data/ex1.csv"
-    TEST_PATH_2 = "./mave/data/ex2.csv"
-    TEST_PATH_3 = "./mave/data/ex3.csv"
-    TEST_PATH_4 = "./mave/data/ex4.csv"
-    TEST_PATH_5 = "./mave/data/ex5.csv"
-    TEST_PATH_6 = "./mave/data/ex6.csv"
-    TEST_PATH_7 = "./mave/data/ex7.csv"
-    TEST_PATH_8 = "./mave/data/ex8.csv"
+    F_1 = "./mave/data/ex4_short_no_weather.csv"
     WEATHER_PATH = "./mave/data/weather_test.csv"
 
-    def test_success(self):
-        self.assertTrue(True)
-
     def test_preprocessor(self):
-        f = open(self.TEST_PATH_1, 'Ur')
-        p = Preprocessor(f, end_frac =0.2)
-        self.assertTrue(p is not None)
-        self.assertTrue(p.X.shape == (18411,8))
-        self.assertTrue(len(p.X) == len(p.y)) 
-        self.assertTrue(len(p.y) == len(p.dts))
-        self.assertTrue(int(p.X[10000][0]) == p.dts[10000].minute)
+        # a basic test of the preprocessor object
+        f = open(self.F_1, 'Ur')
+        o = Preprocessor(f)
+        assert_equal(o.X.shape, (5643, 4))
+        assert_equal(len(o.X), len(o.y)) 
+        assert_equal(len(o.y), len(o.dts))
+        assert_equal(int(o.X[5000][0]), o.dts[5000].minute)
+        assert_equal(int(o.X[5000][1]), o.dts[5000].hour)
+        # test if holiday feature identifies first day of year as holiday
+        assert_equal(o.X[0][3], 3 )
         f.close()
 
-    def test_preprocessor_remove_outliers(self):
-        f = open(self.TEST_PATH_2, 'Ur')
-        p = Preprocessor(f, end_frac=0.05, remove_outliers=False)
-        f.close()
-        f = open(self.TEST_PATH_2, 'Ur')
-        pm = Preprocessor(f, end_frac=0.05, remove_outliers='MultipleValues')
-        f.close()
-        f = open(self.TEST_PATH_2, 'Ur')
-        ps = Preprocessor(f, end_frac=0.05, remove_outliers='SingleValue')
-        for a in (p,pm,ps):
-            self.assertTrue(a is not None)
-            self.assertTrue(len(a.X) == len(a.y)) 
-            self.assertTrue(len(a.y) == len(a.dts))
-            self.assertTrue(int(a.X[1000][0]) == a.dts[1000].minute)
-        self.assertTrue(p.X.shape == (4593,8))
-        self.assertTrue(pm.X.shape == (4590,8))
-        self.assertTrue(ps.X.shape == (4591,8))
-        f.close()
-        
     def test_changepoint_feature(self):
-        f = open(self.TEST_PATH_3, 'Ur')
+        # test changepoint feature 
+        f = open(self.F_1, 'Ur')
         changepoints = [
-                       ("2012/1/29 13:15", Preprocessor.PRE_DATA_TAG),
-                       ("2012/12/20 01:15", Preprocessor.IGNORE_TAG),
-                       ("2013/1/1 01:15", Preprocessor.PRE_DATA_TAG),
-                       ("2013/9/14 23:15", Preprocessor.POST_DATA_TAG),
+                       ("2007/1/14 01:15", Preprocessor.POST_DATA_TAG),
+                       ("2007/1/17 01:15", Preprocessor.IGNORE_TAG),
+                       ("2007/1/18 01:15", Preprocessor.PRE_DATA_TAG),
+                       ("2007/1/31 23:45", Preprocessor.POST_DATA_TAG),
                        ]
-        p = Preprocessor(f,
-                         use_holidays = True, 
-                         changepoints=changepoints)
-        self.assertTrue(len(p.X) == len(p.y)) 
-        self.assertTrue(len(p.y) == len(p.dts))
-        self.assertTrue(int(p.X[10000][0]) == p.dts[10000].minute)
-        self.assertTrue(len(p.X_pre_s) == 55877)
+        o = Preprocessor(f, changepoints=changepoints, use_holidays=False)
+        # test if datetimes correctly dentified as pre/post/ignore 
+        assert_true(datetime(2007, 1, 14, 1, 15) in o.dts_post)
+        assert_true(datetime(2007, 1, 18, 1, 15) in o.dts_pre)
+        assert_true(datetime(2007, 1, 17, 1, 15) in o.dts)
+        assert_false(datetime(2007, 1, 17, 1, 15) in o.dts_pre)
+        assert_false(datetime(2007, 1, 17, 1, 15) in o.dts_post)
+        # test holiday feature not present
+        assert_equal(o.X.shape[1], 3)
+        assert_false('Holiday' in o.feature_names)
         f.close()
 
     def test_model_aggregator(self):
-        f = open(self.TEST_PATH_4, 'Ur')
-        p = Preprocessor(f, test_size=0.75)
-        m = ModelAggregator(X = p.X_pre_s,
-                            y = p.y_pre_s,
-                            y_standardizer = p.y_standardizer)
-        self.assertTrue(m is not None)
-        dummy = m.train("dummy")
-        self.assertTrue(dummy is not None)
+        # test dataset and modelaggregator 
+        f = open(self.F_1, 'Ur')
+        o = Preprocessor(f, test_size=0.5, use_month=True)
+        # test correct number of features present (min,hr,day,hol,mnt)
+        assert_equal(o.X.shape[1], 5)
+        assert_true('Month' in o.feature_names)
+        A = dataset.Dataset(dataset_type='A',
+                            X_s=o.X_pre_s,
+                            X_standardizer=o.X_standardizer,
+                            y_s=o.y_pre_s,
+                            y_standardizer=o.y_standardizer,
+                            dts=o.dts_pre,
+                            feature_names=o.feature_names)
+        m = ModelAggregator(dataset=A)
+        m.train_all(k=3)
         score = m.score()
-        m.train("hour_weekday")
-        score = m.score()
-        self.assertTrue(m.error_metrics.cvrmse < 47.0)
+        assert_true(m.error_metrics.r2 > 0.9)
         f.close()
 
     def test_mnv(self):
-        f = open(self.TEST_PATH_5, 'Ur')
-        changepoints = [
-                       ("2012/1/29 13:15", Preprocessor.PRE_DATA_TAG),
-                       ("2012/12/20 01:15", Preprocessor.IGNORE_TAG),
-                       ("2013/1/1 01:15", Preprocessor.PRE_DATA_TAG),
-                       ("2013/2/14 23:15", Preprocessor.POST_DATA_TAG),
-                       ]
-        mnv = MnV(f, changepoints=changepoints)
-        self.assertTrue(mnv.error_metrics.r2 > 0.25)
+        f = open(self.F_1, 'Ur')
+        mnv = MnV(f)
+        assert_true(mnv.DvsE.r2 > 0.8)
+
+    def test_location(self):
+        o = location.Location(address='berkeley california')
+        assert_equal(o.real_addrs, 'Berkeley, CA, USA')
+        assert_almost_equal(o.lat, 37.87, places=2)
+        assert_almost_equal(o.lon, -122.27, places=2)
     
-    def test_preprocessor_integer_data(self):   
-        f = open(self.TEST_PATH_6, 'Ur')
-        p = Preprocessor(f,use_holidays = True, start_frac=0.4, end_frac=0.6)
-        self.assertTrue(p is not None)
-        self.assertTrue(len(p.X) == 18406)
-        self.assertTrue(p.X.shape == (18406,8))
-        f.close()
-
-    def test_random_forest(self):
-        x = np.random.randint(10000, size=(10000,3))+100
-        y = np.random.randint(10000, size=(10000,))+100
-        random_forest_trainer = trainers.RandomForestTrainer(\
-                                                           search_iterations=20)
-        model = random_forest_trainer.train(x,y)
-        self.assertTrue(random_forest_trainer is not None)
-        X = np.random.randint(10000, size=(10000,3))+200
-        y_predicted = model.predict(X)
-        c = Comparer(y_predicted, y)
-        self.assertTrue(c.r2 < 0.01)
-
-    def test_preprocessor_no_hols(self):
-        f = open(self.TEST_PATH_8, 'Ur')
-        p = Preprocessor(f, start_frac=0.4, end_frac =0.6, use_holidays=False)
-        self.assertTrue(p is not None)
-        self.assertTrue(p.X.shape == (18287,7))
-        self.assertTrue(len(p.X) == len(p.y)) 
-        self.assertTrue(len(p.y) == len(p.dts))
-        self.assertTrue(int(p.X[10000][0]) == p.dts[10000].minute)
-        f.close()
-
     def test_getweather(self):
-        start = datetime.datetime(2012,1,1,0,0)
-        end = datetime.datetime(2012,1,3,0,0)
-        web = location.Weather(start=start,end=end,key=None,\
-                               geocode='SFO',interp_interval='15m',save=False)
-        api = location.Weather(start=start,end=end,key='d3dffb3b59309a05',\
-                               geocode='SFO',interp_interval='15m',save=False)
-        self.assertTrue(web.data != None)
-        self.assertAlmostEqual(float(web.data[0][0]),8.277777777)  
-        self.assertAlmostEqual(float(api.data[0][0]),8.3)  
+        start = datetime(2012,1,1,0,0)
+        end = datetime(2012,1,2,0,0)
+        o = location.Weather(start=start,
+                             end=end,
+                             geocode='SFO',
+                             interp_interval=15,
+                             save=False)
+        assert_equal(o.data[0].shape,(48,))
+        assert_almost_equal(float(o.data[0][0]), 8.277777777)
+        assert_equal(o.interp_data[0].shape,(97,))
+        assert_almost_equal(o.interp_data[-1][-1], 42.75333333)
+  
 
-        
 if __name__ == '__main__':
     unittest.main()
